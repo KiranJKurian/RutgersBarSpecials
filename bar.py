6246e5c6f8f4df0ae97741b44dc70ab3534f835d
jmalmstrom13 <johannamalmstrom13@gmail.com>
2016-11-21 15:52:29 -0500

    Add python code to the repo

---
+++ bar.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/bar/')
+@app.route('/bar/<name>')
+def bar(name=None):
+	return render_template('bar.html', name=name)
+	
+app.run()
