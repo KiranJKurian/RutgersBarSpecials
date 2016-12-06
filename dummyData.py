7bcee11c7bea0fdefb93ee894c3621465af96e41
Vatsal09 <vpan17@gmail.com>
2016-12-05 22:57:41 -0500

    Fixed dummyData

--- dummyData.py
+++ dummyData.py
@@ -1,46 +1,28 @@
-bars = [{"id": 1, "name": "Old Queens", "address": "108 Easton Ave", "zip": 12345}]
+bars = [{"id": 1, "name": "Old Queens", "address": "108 Easton Ave", "zip": 12345}, 
+		{"id": 2, "name": "OB", "address": "35 Blue St", "zip": 64822},
+		{"id": 2, "name": "OB", "address": "35 Blue St", "zip": 64822},
+		{"id": 3, "name": "Golden Rail", "address": "677 FooBar St", "zip": 23455},
+		{"id": 4, "name": "World of Beer", "address": "388 George St", "zip": 10018},
+		{"id": 5, "name": "Best Bar", "address": "375 Easton Ave", "zip": 13495},
+		{"id": 6, "name": "Rutgers Bar", "address": "45 Gray Ave", "zip": 12345},
+		{"id": 7, "name": "Barzzz", "address": "4 Eason Ave", "zip": 12975}]
 
-bar1 = [{"id": 2, "name": "OB", "address": "35 Blue St", "zip": 64822}]
-bars.append(bar1)
-bar2 = [{"id": 3, "name": "Golden Rail", "address": "677 FooBar St", "zip": 23455}]
-bars.append(bar2)
-bar3 = [{"id": 4, "name": "World of Beer", "address": "388 George St", "zip": 10018}]
-bars.append(bar3)
-bar4 = [{"id": 5, "name": "Best Bar", "address": "375 Easton Ave", "zip": 13495}]
-bars.append(bar4)
-bar5 = [{"id": 6, "name": "Rutgers Bar", "address": "45 Gray Ave", "zip": 12345}]
-bars.append(bar5)
-bar6 = [{"id": 7, "name": "Barzzz", "address": "4 Eason Ave", "zip": 12975}]
-bars.append(bar6)
+specials = [{"day":"Monday", "special":"some beer", "price": 4, "bar_id":bars[0]["id"]},
+			{"day":"Monday", "special":"shots", "price": 1, "bar_id":bars[0]["id"]},
+			{"day":"Friday", "special":"LIT", "price": 3, "bar_id":bars[6]["id"]},
+			{"day":"Wednesday", "special":"Teq shots", "price": 1, "bar_id":bars[4]["id"]},
+			{"day":"Saturday", "special":"Mojitos", "price": 4, "bar_id":bars[3]["id"]},
+			{"day":"Sunday", "special":"Vodka shots", "price": 1, "bar_id":bars[2]["id"]},
+			{"day":"Friday", "special":"Imported beer", "price": 4, "bar_id":bars[2]["id"]},
+			{"day":"Friday", "special":"Margarita", "price": 1, "bar_id":bars[1]["id"]},
+			{"day":"Friday", "special":"Margarita", "price": 1, "bar_id":bars[1]["id"]},
+			{"day":"Tuesday", "special":"Daiquiri", "price": 4, "bar_id":bars[5]["id"]},
+			{"day":"Thursday", "special":"Bloody Mary", "price": 1, "bar_id":bars[0]["id"]},
+			{"day":"Friday", "special":"Craft beer", "price": 4, "bar_id":bars[5]["id"]},
+			{"day":"Saturday", "special":"Some shots", "price": 1, "bar_id":bars[1]["id"]},
+			{"day":"Saturday", "special":"Any beer", "price": 4, "bar_id":bars[6]["id"]},
+			{"day":"Wednesday", "special":"Any shot", "price": 1, "bar_id":bars[5]["id"]}]
 
 
 
 
-specials = [{"day":"Monday", "special":"some beer", "price": 4, "bar_id":bars[0]["id"]}]
-
-special1 = [{"day":"Monday", "special":"shots", "price": 1, "bar_id":bars[0]["id"]}]
-specials.append(special1)
-special2 = [{"day":"Friday", "special":"LIT", "price": 3, "bar_id":bars[6]["id"]}]
-specials.append(special2)
-special3 = [{"day":"Wednesday", "special":"Teq shots", "price": 1, "bar_id":bars[4]["id"]}]
-specials.append(special3)
-special4 = [{"day":"Saturday", "special":"Mojitos", "price": 4, "bar_id":bars[3]["id"]}]
-specials.append(special4)
-special5 = [{"day":"Sunday", "special":"Vodka shots", "price": 1, "bar_id":bars[2]["id"]}]
-specials.append(special5)
-special6 = [{"day":"Friday", "special":"Imported beer", "price": 4, "bar_id":bars[2]["id"]}]
-specials.append(special6)
-special7 = [{"day":"Friday", "special":"Margarita", "price": 1, "bar_id":bars[1]["id"]}]
-specials.append(special7)
-special8 = [{"day":"Tuesday", "special":"Daiquiri", "price": 4, "bar_id":bars[5]["id"]}]
-specials.append(special8)
-special9 = [{"day":"Thursday", "special":"Bloody Mary", "price": 1, "bar_id":bars[0]["id"]}]
-specials.append(special9)
-special10 = [{"day":"Friday", "special":"Craft beer", "price": 4, "bar_id":bars[5]["id"]}]
-specials.append(special10)
-special11 = [{"day":"Saturday", "special":"Some shots", "price": 1, "bar_id":bars[1]["id"]}]
-specials.append(special11)
-special12 = [{"day":"Saturday", "special":"Any beer", "price": 4, "bar_id":bars[6]["id"]}]
-specials.append(special2)
-special13 = [{"day":"Wednesday", "special":"Any shot", "price": 1, "bar_id":bars[5]["id"]}]
-specials.append(special13)
