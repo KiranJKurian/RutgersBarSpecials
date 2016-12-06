2bef2a386ca484b27d3dbb8c25a28043e309d940
Vatsal09 <vpan17@gmail.com>
2016-12-05 22:31:23 -0500

    Added dummmydata and edited the schema of the database (removed child class)

--- app/app.py
+++ app/app.py
@@ -13,13 +13,26 @@ class bars(db.Model):
    city = db.Column(db.String(50))
    addr = db.Column(db.String(200)) 
    zip = db.Column(db.String(10))
-   special = db.relationship('special', backref='bars', lazy='dynamic')
+   mon = db.Column(db.String(240))
+   tues = db.Column(db.String(240))
+   weds = db.Column(db.String(240))
+   thurs = db.Column(db.String(240))
+   fri = db.Column(db.String(240))
+   sat = db.Column(db.String(240))
+   sun = db.Column(db.String(240))
 
-   def __init__(self, name, city, addr, zip):
+   def __init__(self, name, city, addr, zip, mon, tues, weds, thurs, fri, sat, sun):
       self.name = name
       self.city = city
       self.addr = addr
       self.zip = zip
+      self.mon = mon
+      self.tues = tues
+      self.weds = weds
+      self.thurs = thurs
+      self.fri = fri
+      self.sat = sat
+      self.sun = sun
       # How would you define the "specials" variable because if it's not defined, the program throws an error ("specials not defined")?
       # I tried to debug using the following and resulted in "[specials]". I am not sure if it's right.
       # http://stackoverflow.com/questions/19606745/flask-sqlalchemy-error-typeerror-incompatible-collection-type-model-is-not
@@ -28,29 +41,6 @@ class bars(db.Model):
 
       #self.specials = specials
      
-
-class special(db.Model):
-   id = db.Column(db.Integer, primary_key = True)
-   mon = db.Column(db.String(240))
-   tues = db.Column(db.String(240))
-   weds = db.Column(db.String(240))
-   thurs = db.Column(db.String(240))
-   fri = db.Column(db.String(240))
-   sat = db.Column(db.String(240))
-   sun = db.Column(db.String(240))
-   bars_id = db.Column(db.Integer, db.ForeignKey(bars.id))
-
-   def __init__(self, mon, tues, weds, thurs, fri, sat, sun):
-      self.mon = mon
-      self.tues = tues
-      self.weds = weds
-      self.thurs = thurs
-      self.fri = fri
-      self.sat = sat
-      self.sun = sun
-    
-
-   
 @app.route('/')
 def show_all():
    return render_template('show_all.html', bars = bars.query.all() )
@@ -61,9 +51,9 @@ def new():
       if not request.form['name'] or not request.form['city'] or not request.form['addr'] or not request.form['zip']:
          flash('Please enter all the fields', 'error')
       else:
-         specials_a = special(request.form['special_mon'], request.form['special_tues'], request.form['special_weds'], request.form['special_thurs'], request.form['special_fri'], request.form['special_sat'], request.form['special_sun']) 
-         bar = bars(request.form['name'], request.form['city'], request.form['addr'], request.form['zip']) 
-         specials_a.bar_id = bar.id;             
+         
+         bar = bars(request.form['name'], request.form['city'], request.form['addr'], request.form['zip'], request.form['special_mon'], request.form['special_tues'], request.form['special_weds'], request.form['special_thurs'], request.form['special_fri'], request.form['special_sat'], request.form['special_sun']) 
+                    
          db.session.add(bar)
          db.session.commit()
          flash('Record was successfully added')
--- dummyData.py
+++ dummyData.py
@@ -1,5 +1,46 @@
-data = [{ "name": "Old Queens", "monday": ["$2 LITs", "$4 Yeunglings"] }]
-data.append({ 'name': "Knight Club", 'monday': ["$999 shot of vodka"] })
+bars = [{"id": 1, "name": "Old Queens", "address": "108 Easton Ave", "zip": 12345}]
 
-for bar in data:
-	print "Name: %s, Monday Specials: %s" % (bar['name'], bar['monday'])
+bar1 = [{"id": 2, "name": "OB", "address": "35 Blue St", "zip": 64822}]
+bars.append(bar1)
+bar2 = [{"id": 3, "name": "Golden Rail", "address": "677 FooBar St", "zip": 23455}]
+bars.append(bar2)
+bar3 = [{"id": 4, "name": "World of Beer", "address": "388 George St", "zip": 10018}]
+bars.append(bar3)
+bar4 = [{"id": 5, "name": "Best Bar", "address": "375 Easton Ave", "zip": 13495}]
+bars.append(bar4)
+bar5 = [{"id": 6, "name": "Rutgers Bar", "address": "45 Gray Ave", "zip": 12345}]
+bars.append(bar5)
+bar6 = [{"id": 7, "name": "Barzzz", "address": "4 Eason Ave", "zip": 12975}]
+bars.append(bar6)
+
+
+
+
+specials = [{"day":"Monday", "special":"some beer", "price": 4, "bar_id":bars[0]["id"]}]
+
+special1 = [{"day":"Monday", "special":"shots", "price": 1, "bar_id":bars[0]["id"]}]
+specials.append(special1)
+special2 = [{"day":"Friday", "special":"LIT", "price": 3, "bar_id":bars[6]["id"]}]
+specials.append(special2)
+special3 = [{"day":"Wednesday", "special":"Teq shots", "price": 1, "bar_id":bars[4]["id"]}]
+specials.append(special3)
+special4 = [{"day":"Saturday", "special":"Mojitos", "price": 4, "bar_id":bars[3]["id"]}]
+specials.append(special4)
+special5 = [{"day":"Sunday", "special":"Vodka shots", "price": 1, "bar_id":bars[2]["id"]}]
+specials.append(special5)
+special6 = [{"day":"Friday", "special":"Imported beer", "price": 4, "bar_id":bars[2]["id"]}]
+specials.append(special6)
+special7 = [{"day":"Friday", "special":"Margarita", "price": 1, "bar_id":bars[1]["id"]}]
+specials.append(special7)
+special8 = [{"day":"Tuesday", "special":"Daiquiri", "price": 4, "bar_id":bars[5]["id"]}]
+specials.append(special8)
+special9 = [{"day":"Thursday", "special":"Bloody Mary", "price": 1, "bar_id":bars[0]["id"]}]
+specials.append(special9)
+special10 = [{"day":"Friday", "special":"Craft beer", "price": 4, "bar_id":bars[5]["id"]}]
+specials.append(special10)
+special11 = [{"day":"Saturday", "special":"Some shots", "price": 1, "bar_id":bars[1]["id"]}]
+specials.append(special11)
+special12 = [{"day":"Saturday", "special":"Any beer", "price": 4, "bar_id":bars[6]["id"]}]
+specials.append(special2)
+special13 = [{"day":"Wednesday", "special":"Any shot", "price": 1, "bar_id":bars[5]["id"]}]
+specials.append(special13)
