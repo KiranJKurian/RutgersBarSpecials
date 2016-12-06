1e0ed563b523253f8ee7408e5b4f5c706df4c3ef
jmalmstrom13 <johannamalmstrom13@gmail.com>
2016-12-05 19:21:24 -0500

    Added stuff

--- bar.py
+++ bar.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import pypyodbc
 app = Flask(__name__)
 
 @app.route('/bar/')
 @app.route('/bar/<name>')
 def bar(name=None):
+	connection = pypyodbc.connect('Server=bars3.sqlite;''uid=Questlove-Magic;')
+	cursor = connection.cursor()
+	SQLCommand = ("SELECT name")
+	results = cursor.fetchall()
+	print("The bar's name is " + results[0])
+	connection.close()
 	return render_template('bar.html', name=name)
 
 if __name__ == '__main__':	
