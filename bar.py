74b642373709f49602505fc555d20786299f090c
jmalmstrom13 <johannamalmstrom13@gmail.com>
2016-12-14 17:28:17 -0500

    Python and HTML

--- bar.py
+++ bar.py
@@ -1,21 +1,15 @@
 from flask import Flask
 from flask import render_template
-import sqlite3
+
 app = Flask(__name__)
 
 @app.route('/bar/')
 @app.route('/bar/<name>')
 def bar(name=None):
-	
+	from dummyData import bars
+	name = bars['name']
 	return render_template('bar.html', name=name)
-	sqlite_file = '/Users/Johanna/Documents/GitHub/RutgersBarSpecials/app/bars.sqlite3'
-	table_name= 'bars'
-	connection = sqlite3.connect(sqlite_file)
-	c = connection.cursor()
-	c.execute('PRAGMA TABLE_INFO({})'.format(table_name))
-	names = [tup[1] for tup in c.fetchall()]
-	print(names)
-	connection.close()
+
 
 if __name__ == '__main__':	
 	app.run()
