754d0c6b26ab7b208e9318c7541f8df0f83cf13c
jmalmstrom13 <johannamalmstrom13@gmail.com>
2017-01-04 15:54:49 -0500

    Harcoded in specials

--- bar.py
+++ bar.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 @app.route('/bar/<name>')
 def bar(name=None):
 	from dummyData import bars
-	name = bars[0]['name']
-	barOne = bars[0]
-	return render_template('bar.html', name=name, barOne = barOne)
+	from dummyData import specials
+	barsAr = [None] * 8
+	for x in range(0,8):
+		barsAr[x] = bars[x]
+	specialsAr = [None] * 15
+	for x in range (0,15):
+		specialsAr[x] = specials[x]
+	return render_template('bar.html', barsAr = barsAr, specialsAr = specialsAr)
 
 
 if __name__ == '__main__':
